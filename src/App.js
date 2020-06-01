b0a59b85800838ff4bc4b4f1a9ece84d4b47fcd8
luis guzman <antilou86@gmail.com>
2020-06-01 17:56:16 -0400

    basic routing set up, template of log in and registration pages set up as well. need to flesh them out more.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React from 'react';
+import {Route, BrowserRouter as Router, Switch} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React or whatever
+          </a>
+        </header>
+        <Switch>
+              <Route exact path="/" component={}/>
+              <Route path="/registration" component={}/>
+              <Route path="/login" component={}/>
+              <Route path="/sellables" component={}/>
+              <Route path="/profile" component ={}/>
+        </Switch>
+        {/* footer here */}
+      </div>
+    </Router>
   );
 }
 
