53819fb7c5eaa1ea0e288a6cebd4fb61bfd0d83f
luis guzman <antilou86@gmail.com>
2020-06-13 21:37:33 -0400

    fleshed out more of the individual item cards. still needs styling.

--- src/components/Bugs_and_fish_card.js
+++ src/components/Bugs_and_fish_card.js
@@ -10,10 +10,11 @@ const Bugs_and_fish_card = (props) => {
             <img src={`${props.image_url}`}>
                 <h3>{props.sell_price}</h3>
             </img>
-
-            {/*checkHemisphere() will need to return true if the logged in user is in the northern or southern hemisphere
-            since this was built specifically for my friends and family it is going to default to northern for now*/}
             <p>Location: {`${props.location}`}</p>
+            {/*season availability will eventually use a function called checkHemisphere() that'll return true if the 
+            logged in user is in the northern hemisphere or false for southern hemisphere, allowing me to toss into a
+            ternery that will render northern or southern hemisphere props based on relevant user location. since this
+            app was built specifically for my friends and family it is going to default to northern for now*/}
             <p>Available: {`${props.northern_season}`}</p>
             {props.northern_availability=="N/A" ? <p>Hours: Unknown</p> :<p>Hours: {props.northern_availability}</p> }
         </div>
