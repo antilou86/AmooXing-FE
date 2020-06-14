2be6045471caa25b6d39cc535926ebd1b6b881f2
luis guzman <antilou86@gmail.com>
2020-06-13 21:57:30 -0400

    still need to configure search bar and do styling for returned cards. got api call and a way to return the list all set up.

--- src/components/Bugs_and_fish.js
+++ src/components/Bugs_and_fish.js
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import Bugs_and_fish_card from './Bugs_and_fish_card';
 
 const Bugs_and_fish = () => {
+    //load data into a state object.(useState)
+    const [items, setItems] = useState([]);
+
     //pull in all data on pageload. (useEffect)
-        //load that data into a state object.(useState)
+    useEffect(() => {
+        const getItems = () => {
+            axios
+                .get('https://amoo-xing.herokuapp.com/sellables')
+                .then(response => {
+                    setItems(response.data);
+                })
+                .catch(error => {
+                    console.error('Server Error', error);
+                });
+        }
+        getItems();
+    }, [])
 
     return (
         // container for the whole ass page.
-        <div>
+        <div className = "item-list">
             {/* a div to hold our filter bar. */}
             <div></div>
 
             {/* div to contain the results of the api call*/}
             <div>
-            {/*map over props and return a collectables (fish/bug) card 
-            for each item in state*/}
+                {/*map over props and return a collectables (fish/bug) card 
+                for each item in state*/}
+                {items.map(item => (
+                    <Bugs_and_fish_card key={item.id} item={item}/>
+                ))}
             </div>
         </div>
     )
--- src/components/Bugs_and_fish_card.js
+++ src/components/Bugs_and_fish_card.js
@@ -1,22 +1,22 @@
 import React from 'react';
 
-const Bugs_and_fish_card = (props) => {
-
+const Bugs_and_fish_card = ({ item }) => {
+    const { name, image_url, sell_price, location, northern_season, northern_availability } = item;
     return (
         // container
         <div>
             {/*will this look horrible?*/}
-            <h2>{props.name}</h2>
-            <img src={`${props.image_url}`}>
-                <h3>{props.sell_price}</h3>
+            <h2>{name}</h2>
+            <img src={`${image_url}`}>
+                <h3>{sell_price}</h3>
             </img>
-            <p>Location: {`${props.location}`}</p>
+            <p>Location: {`${location}`}</p>
             {/*season availability will eventually use a function called checkHemisphere() that'll return true if the 
             logged in user is in the northern hemisphere or false for southern hemisphere, allowing me to toss into a
-            ternery that will render northern or southern hemisphere props based on relevant user location. since this
+            ternery that will render northern or southern hemisphere based on relevant user location. since this
             app was built specifically for my friends and family it is going to default to northern for now*/}
-            <p>Available: {`${props.northern_season}`}</p>
-            {props.northern_availability=="N/A" ? <p>Hours: Unknown</p> :<p>Hours: {props.northern_availability}</p> }
+            <p>Available: {`${northern_season}`}</p>
+            {northern_availability=="N/A" ? <p>Hours: Unknown</p> :<p>Hours: {northern_availability}</p> }
         </div>
     )
 }
