dce8730cbd81acaaffd99ddec19d398ecc2e998e
luis guzman <antilou86@gmail.com>
2020-06-13 21:34:03 -0400

    fleshed out more of the individual item cards. still needs styling.

--- src/components/Bugs_and_fish_card.js
+++ src/components/Bugs_and_fish_card.js
@@ -1,15 +1,21 @@
 import React from 'react';
 
 const Bugs_and_fish_card = (props) => {
+
     return (
         // container
         <div>
-            {/* a div to hold our filter bar. */}
-            <div></div>
-            {/* div to contain the results */}
-            <div>
-            {/*map over props and return a collectables (fish/bug) card*/}
-            </div>
+            {/*will this look horrible?*/}
+            <h2>{props.name}</h2>
+            <img src={`${props.image_url}`}>
+                <h3>{props.sell_price}</h3>
+            </img>
+
+            {/*checkHemisphere() will need to return true if the logged in user is in the northern or southern hemisphere
+            since this was built specifically for my friends and family it is going to default to northern for now*/}
+            <p>Location: {`${props.location}`}</p>
+            <p>Available: {`${props.northern_season}`}</p>
+            {props.northern_availability=="N/A" ? <p>Hours: Unknown</p> :<p>Hours: {props.northern_availability}</p> }
         </div>
     )
 }
