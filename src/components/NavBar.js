b5dd571a73039f2d163a9399af2c9ffaa50b7d98
luis guzman <antilou86@gmail.com>
2020-08-11 17:56:04 -0400

    added navBar component still need to impliment

---
+++ src/components/NavBar.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom"
+import styled from 'styled-components';
+
+const NavContainer = styled.div`
+    width: 100%;
+    display: flex;
+    flex-flow: row wrap;
+`
+
+const NavBar = () => {
+    return (
+        <NavContainer>
+            <h1>Where To?</h1>
+            <div>
+                <Link to="/">Home</Link>
+            </div>
+            <div>
+                <Link to="/sellables">Creatures</Link>
+            </div>
+            <div>
+                <Link to="/villagers">Villagers</Link>
+            </div>
+            {/* <div>
+                <Link to="/profile">Profile</Link>
+            </div> */}
+        </NavContainer>
+    )
+}
+
+export default NavBar
